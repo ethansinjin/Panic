7ca1d46b7ec057799fd8bc349538c35c57787f43
Ethan Gill <ethan.gill@me.com>
2016-10-08 03:10:40 -0400

    add basic button

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,34 @@
 // Your code goes here
 
-var vinElem = document.getElementById('vin');
-gm.info.getVehicleConfiguration(function(data) {
-  vinElem.innerHTML = gm.info.getVIN();
-});
+// var vinElem = document.getElementById('vin');
+// gm.info.getVehicleConfiguration(function(data) {
+//   vinElem.innerHTML = gm.info.getVIN();
+// });
+
+var navigationButton = document.getElementById('navigationButton');
+
+var dest = {
+  address: '400 Renaissance Center Drive, Detroit, Michigan 48243'
+}
+
+function navigateTest() {
+  gm.nav.setDestination(navigateSuccess, dest, true);
+  document.getElementById("navigationButton").innerHTML = "in progress";
+}
+
+function navigateSuccess() {
+  console.log("success");
+  var id = gm.voice.startTTS(ttsEnd, 'Keep your eyes on the road, your hands up on the wheel');
+  navigationButton.hidden = true;
+  document.getElementById("result").innerHTML = "success";
+  //TODO: voice "starting navigation"
+}
+
+// function navigateError(err) {
+//   //var id = gm.voice.startTTS(ttsEnd, 'Navigation Failed.');
+//   document.getElementById("result").innerHTML = "Failed";
+// }
+
+function ttsEnd() {
+  //TODO: tts finished callback
+}
